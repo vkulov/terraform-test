838d4be29b30578894c6067450d9bb62699c3fd7
vesko <veselin@kulov.net>
2016-11-06 23:20:49 +0200

    associate aws deployment to autoscaling group

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -111,11 +111,13 @@ resource "aws_codedeploy_deployment_group" "main" {
     deployment_group_name = "vkulov"
     service_role_arn = "${aws_iam_role.vkulov_deploy_role.arn}"
 
-    ec2_tag_filter {
-        key = "filterkey"
-        type = "KEY_AND_VALUE"
-        value = "filtervalue"
-    }
+    autoscaling_groups = "${var.name}"
+
+    #ec2_tag_filter {
+    #    key = "filterkey"
+    #    type = "KEY_AND_VALUE"
+    #    value = "filtervalue"
+    #}
 
     #trigger_configuration {
     #    trigger_events = ["DeploymentFailure"]
